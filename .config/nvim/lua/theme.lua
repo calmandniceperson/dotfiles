603449add29240cb2c11c8ef89d50dc832a43e37
Michael Koeppl <koeppl.michael@outlook.com>
2021-07-11 18:25:58 +0200

    Set Tokyo Night colorscheme background color to darker grey

--- .config/nvim/lua/theme.lua
+++ .config/nvim/lua/theme.lua
@@ -1,6 +1,7 @@
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = true
+vim.g.tokyonight_colors = { bg = '#131313' }
 vim.cmd[[colorscheme tokyonight]]
 
 
