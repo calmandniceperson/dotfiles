0170ee6f9c8091ded88d708bb0e0de26737d9a6c
Michael Koeppl <koeppl.michael@outlook.com>
2021-07-11 18:25:44 +0200

    Enable indentation and incremental selection for treesitter

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -119,6 +119,18 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = { }, -- List of parsers to ignore installing
   highlight = { enable = true, use_languagetree = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = {
+    enable = true
+  },
 }
 -------------------------------------------------------------------------------
 
